b87381569209e0611133ffedfc11034d6c5a7189
Eduardo Rodriguez <eduardoalbertorg@gmail.com>
2019-08-04 15:10:38 -0500

    Learn about list operations

---
+++ list_operation.py
@@ -0,0 +1,12 @@
+emplist1 = list()
+emplist2 = []
+print(emplist1)
+emplist1.append(9)
+emplist1.append(10)
+print(emplist1)
+print(emplist2)
+emplist2.extend(['a', 'b', 'c'])
+print(emplist2)
+e = emplist2.pop()
+print(emplist2)
+print(e)
