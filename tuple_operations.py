06548030dbcdfa87de3ea9e9f9f4519fa4db69d6
Eduardo Rodriguez <eduardoalbertorg@gmail.com>
2019-08-04 20:03:05 -0500

    Learn about tuples

---
+++ tuple_operations.py
@@ -0,0 +1,14 @@
+# Create an empty tuple 'tup1' using tuple operation.
+tup1 = tuple()
+# Print 'tup1'.
+print(tup1)
+# Create another tuple 'tup2', by passing 'Welcome' string as argument to tuple function.
+tup2 = tuple('Welcome')
+# Print 'tup2'.
+print(tup2)
+# Find and print the count of character 'e' in 'tup2'.
+print(tup2.count('e'))
+# Determine the index of character 'e' in 'tup2' and print it.
+print(tup2.index('e'))
+# Find the length of tuple 'tup2' and print it.
+print(len(tup2))
