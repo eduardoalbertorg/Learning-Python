41a16b31645fd5e7f3a6362ba6f7db2f349d5e7b
Eduardo Rodriguez <eduardoalbertorg@gmail.com>
2019-09-10 20:43:06 -0500

    Modify Point class __add__ method to return a new point when p1+p2

--- Exercises_using_OOP/_4_1_point_class.py
+++ Exercises_using_OOP/_4_1_point_class.py
@@ -1,15 +1,28 @@
-'''
-Define the class Point that represents x, y, and z coordinates of 3D coordinate system.
+import math
 
-Hint : Define the initializer method, __init__ that takes three values and assigns them to attributes x, y and z respectively.
-Now create an object p1 = Point(4, 2, 9) and print it using the statement print(p1). You may type python3.5 to use the latest version for the exercise
-'''
 
 class Point(object):
+    '''
+    Define the class Point that represents x, y, and z coordinates of 3D coordinate system.
+
+    Hint : Define the initializer method, __init__ that takes three values and assigns them to attributes x, y and z respectively.
+    Now create an object p1 = Point(4, 2, 9) and print it using the statement print(p1). You may type python3.5 to use the latest version for the exercise
+    '''
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
+
+
+    def __add__(self, another_point):
+        '''
+        Improvise Point class definition such that,
+        adding any two Point objects using + operator, results in a new Point object with corresponding x, y and z values being added.
+        For e.g: Point(1, 2, 3) + Point(2, 3, 4) -> Point(3, 5, 7)
+        Hint : Define __add__ method inside the class Point.
+        '''
+        return Point(self.x + another_point.x, self.y + another_point.y, self.z + another_point.z)
+
 
 
     def __str__(self):
